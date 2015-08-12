40bfa6b14ecc22dbd3fa8c6b89a2f877517d8105
Jeff Austin & Tyler Deem <student@epicodus.com>
2015-08-12 11:58:38 -0700

    Added two more tests to evaluate if input1 and input2 are equal and a two tests to evalute differences between input1 and input2 with an expected outcome

--- src/RockPaperScissors.php
+++ src/RockPaperScissors.php
@@ -4,6 +4,14 @@
     {
         function rockPaperScissorsGame ( $input1, $input2 )
         {
-          
+          $input1 = strtolower($input1);
+          $input2 = strtolower($input2);
+
+          if ($input1 === $input2)
+          {
+              return "Draw";
+          }else if ($input1 === "rock" && $input2 === "scissors") {
+              return "Player 1";
+          }
         }
     }
--- tests/RockPaperScissorsTest.php
+++ tests/RockPaperScissorsTest.php
@@ -14,5 +14,41 @@
 
             $this->assertEquals("Draw", $result);
          }
+
+         function test_rockPaperScissorsGame_scissors_scissors()
+         {
+
+             $test_RockPaperScissors = new RockPaperScissors;
+             $input1 = "scissors";
+             $input2 = "scissors";
+
+             $result = $test_RockPaperScissors->rockPaperScissorsGame ($input1, $input2);
+
+             $this->assertEquals("Draw", $result);
+          }
+
+         function test_rockPaperScissorsGame_paper_paper()
+         {
+
+             $test_RockPaperScissors = new RockPaperScissors;
+             $input1 = "paper";
+             $input2 = "paper";
+
+             $result = $test_RockPaperScissors->rockPaperScissorsGame ($input1, $input2);
+
+             $this->assertEquals("Draw", $result);
+          }
+
+          function test_rockPaperScissorsGame_rock_scissors1()
+          {
+
+              $test_RockPaperScissors = new RockPaperScissors;
+              $input1 = "rock";
+              $input2 = "scissors";
+
+              $result = $test_RockPaperScissors->rockPaperScissorsGame ($input1, $input2);
+
+              $this->assertEquals("Player 1", $result);
+           }
     }
 ?>
